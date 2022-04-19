8403fe7781bbb15aff44db058bf1cc9a4b2493ee
Steve Daenen <stevedaenen@hotmail.com>
2022-04-19 13:20:28 +0200

    4

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -3,18 +3,27 @@ package be.vdab.goededoel.domain;
 import java.math.BigDecimal;
 
 public class GoedeDoel {
-	private final String naam;
-	private BigDecimal opbrengst = BigDecimal.ZERO;
+    private final String naam;
+    private BigDecimal opbrengst = BigDecimal.ZERO;
 
-	public GoedeDoel(String naam) {
-		this.naam = naam;
-	}
+    public GoedeDoel(String naam) {
+        this.naam = naam;
+    }
 
-	public String getNaam() {
-		return naam;
-	}
+    public String getNaam() {
+        return naam;
+    }
 
-	public BigDecimal getOpbrengst() {
-		return opbrengst;
-	}
+    public BigDecimal getOpbrengst() {
+        return opbrengst;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof GoedeDoel) {
+            var ander = (GoedeDoel) object;
+            return naam.equalsIgnoreCase(ander.naam);
+        }
+        return false;
+    }
 }
