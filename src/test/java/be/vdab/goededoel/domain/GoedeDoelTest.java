d3c654495635f49d9b96da67883425774242bb7a
Steve Daenen <stevedaenen@hotmail.com>
2022-04-19 13:25:46 +0200

    5

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -6,18 +6,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GoedeDoelTest {
-	private static final String NAAM = "CliniClowns";
-	private GoedeDoel doel;
+    private static final String NAAM = "CliniClowns";
+    private GoedeDoel doel;
 
-	@BeforeEach
-	void beforeEach() {
-		doel = new GoedeDoel(NAAM);
-	}
+    @BeforeEach
+    void beforeEach() {
+        doel = new GoedeDoel(NAAM);
+    }
 
-	@Test
-	void getNaam() {
-		assertThat(doel.getNaam()).isEqualTo(NAAM);
-	}
+    @Test
+    void getNaam() {
+        assertThat(doel.getNaam()).isEqualTo(NAAM);
+    }
 
-	@Test void eenNieuwDoelHeeftNogGeenOpbrengst() { assertThat(doel.getOpbrengst()).isZero(); }
+    @Test
+    void eenNieuwDoelHeeftNogGeenOpbrengst() {
+        assertThat(doel.getOpbrengst()).isZero();
+    }
+
+    @Test
+    void doelenMetDezelfdeNaamZijnGelijk() {
+        assertThat(doel).isEqualTo(new GoedeDoel(NAAM));
+    }
+
+    @Test
+    void doelenMetVerschillendeNaamZijnVerschillend() {
+        assertThat(doel).isNotEqualTo(new GoedeDoel("WWF"));
+    }
+
 }
